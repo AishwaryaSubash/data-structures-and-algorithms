f72197c073a44a7895f7303f6b11cb1cc3a086ff
AishwaryaSubash <riya129ash@gmail.com>
2024-04-04 09:37:22 +0530

    Java codes pushed into a different repo

--- Java/Assignments/PrintEvenCells.java
+++
@@ -1,9 +0,0 @@
-public class PrintEvenCells {
-    public static void main(String[] args) {
-        int[] array = { 10, 20, 30, 40, 50 };
-        System.out.println("Values in even cells in the array:");
-        for (int i = 0; i < array.length; i += 2) {
-            System.out.println("Index " + i + ": " + array[i]);
-        }
-    }
-}
